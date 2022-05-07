0aae00718c0720d2d96fba21b7429faa8334d850
ADIKOLORD <adiko23082004@gmail.com>
2022-05-07 19:43:51 +0600

    feature/add_model_in_product/Product_Category_Status/add_admin_and_setting

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,116 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
-# Register your models here.
+from product.models import *
+
+# class ModelAdmin(admin.ModelAdmin):
+'''
+class ModelAdmin(admin.ModelAdmin):
+    fields -> Кайсы поляларды толтурганга болот. Же добавить эткенде кайсылар корунсун
+
+    exclude -> Кайсы полялар добавить эткенде корунбосун
+
+    readonly_fields -> Кайсыларды изменить этсе болбойт! 
+
+    list_display -> Отображение на панеле
+
+    list_display_links -> Переобразовать в ссылку
+
+    search_fields -> Поиск болгондо кайсылардан издеш керек!
+
+    list_editable -> Панелде сразу редактирование кылса болот
+
+    list_filter -> Фильтр для отображения
+
+    list_per_page -> Бир страницада канча запись болуш керек
+
+    prepopulated_fields -> Ключ болуп турган поляга, значениеде турган полянын ичиндеги жазылат!
+
+    actions -> Кайсы дествиялар болуш керек
+
+    # Добавление дествии
+    @admin.action(description='Отображение на панеле действии')
+    def имя_что_делает(self, request, qs: QuerySet):
+        pass
+
+'''
+
+
+class ProductAdmin(admin.ModelAdmin):
+    '''Для настройки админской панели!'''
+
+    # exclude -> Кайсы полялар добавить эткенде корунбосун
+    exclude = ['watch', 'pub_date']
+
+    # readonly_fields -> Кайсыларды изменить этсе болбойт!
+    readonly_fields = []  # watch
+
+    # list_display -> Отображение на панеле
+    list_display = ('title', 'price', 'category', 'watch')
+
+    # list_display_links -> Переобразовать в ссылку
+    list_display_links = ['title', 'category']
+
+    # search_fields -> Поиск болгондо кайсылардан издеш керек!
+    search_fields = ('title', 'description',)
+
+    # list_editable -> Панелде сразу редактирование кылса болот
+    # list_editable = ('price',)
+
+    # list_filter -> Фильтр для отображения
+    list_filter = ('category', 'status')
+
+    # list_per_page -> Бир страницада канча запись болуш керек
+    list_per_page = 15
+
+    # actions -> Кайсы дествиялар болуш керек
+    """
+    actions = ['set_category_man',
+               'set_category_woman',
+               'set_category_sport',
+               'set_category_electric',
+               'set_clone_model',
+               ]
+
+    # Добавление дествии
+    @admin.action(description='Установить категорию Sport')
+    def set_category_sport(self, request, qs: QuerySet):
+        count_updates = qs.update(category=3)
+        # self.message_user -> Функция болгондо кандай сообщение чыгарына жооп берет.
+        self.message_user(request,
+                          f'Было обновлено {count_updates} записей', )
+
+    @admin.action(description='Установить категорию Man')
+    def set_category_man(self, request, qs: QuerySet):
+        qs.update(category=1)
+
+    @admin.action(description='Установить категорию Woman')
+    def set_category_woman(self, request, qs: QuerySet):
+        qs.update(category=2)
+
+    @admin.action(description='Установить категорию Electric')
+    def set_category_electric(self, request, qs: QuerySet):
+        qs.update(category=4)
+
+    @admin.action(description='Копировать как новый модель')
+    def set_clone_model(self, request, qs: QuerySet):
+        for object in qs:
+            object.id = None
+            object.save()
+    """
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # list_display -> Отображение на панеле
+    list_display = ['title', 'parent']
+    # search_fields -> Поиск болгондо кайсылардан издеш керек!
+    search_fields = ['title']
+    # list_filter -> Фильтр для отображения
+    list_filter = ['parent']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Status)
--- product/models.py
+++ product/models.py
@@ -1,7 +1,61 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 '''
 Product 
 Category
-Discount
+Status
 '''
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='Product/%Y/%m/%d', blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    old_price = models.IntegerField(validators=[MinValueValidator(0)], blank=True)
+    pub_date = models.DateField(auto_now=True)
+    status = models.ForeignKey('Status', on_delete=models.CASCADE)
+    watch = models.IntegerField(default=0)
+
+    # wishlist = models.ManyToManyField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    BIGCATEGORIES = (
+        ('1', 'TOP'),
+        ('2', 'BOTTOM'),
+        ('3', 'CLOTHING'),
+        ('4', 'ACCESSORIES'),
+    )
+    parent = models.CharField(max_length=1,
+                              choices=BIGCATEGORIES,
+                              default=3,
+                              verbose_name='Группа', )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Status(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
